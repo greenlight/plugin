bd9db9a9c4e34d07e250805026c29d0047797f70
Ahmad Nassri <ahmad@ahmadnassri.com>
2018-04-17 21:02:45 -0400

    test(fullname): removing tag argument and updating test to match

--- test/fullname.js
+++ test/fullname.js
@@ -7,13 +7,13 @@ test('fullname:success', async assert => {
 
   const docker = new Docker('greenlight/plugin')
 
-  assert.same(docker.fullname, 'greenlight/plugin:latest')
+  assert.same(docker.fullname, 'greenlight/plugin')
 })
 
 test('fullname:with-registry', async assert => {
   assert.plan(1)
 
-  const docker = new Docker('greenlight/plugin, {}, { registry: 'foobar' })
+  const docker = new Docker('greenlight/plugin', {}, { registry: 'foobar' })
 
   assert.same(docker.fullname, 'foobar/greenlight/plugin')
 })
